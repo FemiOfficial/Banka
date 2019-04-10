e35b87648b152638f5a0cedda04f39e3e69d44d5
unknown <alayesanmifemi@gmail.com>
2019-04-10 23:47:12 +0100

    feature(user sign up endpoint): remove duplicate files delete duplicate files

--- server/routes/user.auth.routes.js
+++ server/routes/user.auth.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import UsersControllers from '../controllers/users.auth.controllers';
 import UserValidations from '../middlewares/validations/users.validations';
-import trimInput from '../middlewares/validations/trimInput';
+import trimInput from '../middlewares/validations/trimInputs';
 
 const router = express.Router();
 
 router.post('/signup', UserValidations.checkCreateAccountBody, trimInput, UsersControllers.createUser);
+
+router.post('/signin', UserValidations.checkLoginBody, trimInput, UsersControllers.loginUser);
 
 export default router;
